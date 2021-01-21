bc5ec88bcbe0cb1bf0adf5b7ac36beb91b35e970
박인성 (Bahk InSung) <insung.park123@gmail.com>
2021-01-21 19:53:07 +0900

    사진 변-경
    왜냐하면 리드미에서 tyler.png 사진을 활용했기 때문..

--- Go_for_it/nemtion-opencv-2/plot_hog.py
+++ Go_for_it/nemtion-opencv-2/plot_hog.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from skimage.feature import hog
 from skimage import data, exposure
 
-image = plt.imread('cherrybomb.png')
+image = plt.imread('tyler.png')
 
 fd, hog_image = hog(image, orientations=8, pixels_per_cell=(16,16), 
         cells_per_block=(1,1), visualize=True, multichannel=True)
